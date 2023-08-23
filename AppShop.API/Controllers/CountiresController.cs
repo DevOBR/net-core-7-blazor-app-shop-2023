4e6c3ffc672e83876f72641bd2cafada5c2af147
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2023-08-22 20:30:10 -0600

    Adding Cuntries API Controller

---
+++ AppShop.API/Controllers/CountiresController.cs
@@ -0,0 +1,34 @@
+﻿using AppShop.API.Data;
+using AppShop.Share.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppShop.API.Controllers
+{
+    [ApiController]
+	[Route("/api/countries")]
+	public class CountiresController : ControllerBase
+	{
+        private readonly DataContext _context;
+
+        public CountiresController(DataContext context)
+		{
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody]Country country)
+        {
+            await _context.Countries.AddAsync(country).ConfigureAwait(false);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+            return Ok(country);
+        }
+    }
+}
+
