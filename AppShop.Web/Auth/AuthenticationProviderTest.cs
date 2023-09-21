b67ddf172dfa0d81137b65b4807aec0928d1a3e5
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2023-09-20 22:14:15 -0600

    Adding Security Hardocoded on Web

---
+++ AppShop.Web/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace AppShop.Web.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var adminUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Admin"),
+                new Claim("LastName", "Admin"),
+                new Claim(ClaimTypes.Name, "admin@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            }
+            , authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(adminUser)));
+        }
+    }
+}
+
