9017cb64d62bc8ac739a3b6f44c2ba954baeefa6
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2023-10-30 20:05:09 -0600

    Adding Resend Confirmation User Registration

---
+++ AppShop.Share/DTOs/EmailDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AppShop.Share.DTOs
+{
+    public class EmailDTO
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [EmailAddress(ErrorMessage = "You must enter a valid mail.")]
+        public string Email { get; set; } = null!;
+    }
+}
+
