0f624b1a69fb582604808df2784f3973cccd1af8
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2023-11-23 18:44:42 -0600

    Add Prodct Create

--- AppShop.API/Helper/QueryableExtensions.cs
+++ AppShop.API/Helper/QueryableExtensions.cs
@@ -7,9 +7,12 @@ namespace AppShop.API.Helper
         public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable,
             PaginationDTO pagination)
         {
+            if (pagination.RecordsNumber is -1)
+                return queryable;
+
             return queryable
-                .Skip((pagination.Page - 1) * pagination.RecordsNumber)
-                .Take(pagination.RecordsNumber);
+                    .Skip((pagination.Page - 1) * pagination.RecordsNumber)
+                    .Take(pagination.RecordsNumber);
         }
     }
 }
